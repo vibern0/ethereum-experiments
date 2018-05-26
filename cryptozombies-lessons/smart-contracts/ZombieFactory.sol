c3a396777a0bdbbee4cac62b9cec7bc5a63c0bb6
Bernardo Vieira <bernardo5@live.com.pt>
2018-05-26 23:56:29 +0100

    zoombie feeding

--- cryptozombies-lessons/smart-contracts/ZombieFactory.sol
+++ cryptozombies-lessons/smart-contracts/ZombieFactory.sol
@@ -14,10 +14,15 @@ contract ZombieFactory {
 
     Zombie[] public zombies;
 
-    function _createZombie(string _name, uint _dna) private {
+    mapping (uint => address) public zombieToOwner;
+    mapping (address => uint) ownerZombieCount;
+
+    function _createZombie(string _name, uint _dna) internal {
         uint id = zombies.push(Zombie(_name, _dna)) - 1;
+        zombieToOwner[id] = msg.sender;
+        ownerZombieCount[msg.sender]++;
         NewZombie(id, _name, _dna);
-    } 
+    }
 
     function _generateRandomDna(string _str) private view returns (uint) {
         uint rand = uint(keccak256(_str));
@@ -25,7 +30,9 @@ contract ZombieFactory {
     }
 
     function createRandomZombie(string _name) public {
+        require(ownerZombieCount[msg.sender] == 0);
         uint randDna = _generateRandomDna(_name);
+        randDna = randDna - randDna % 100;
         _createZombie(_name, randDna);
     }
 
