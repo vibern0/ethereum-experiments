e120ade72763892503895c8ede055f063644d593
Bernardo Vieira <bernardo5@live.com.pt>
2018-07-03 18:50:58 +0100

    added cancel rent method a check is valid

--- rent-a-place/truffle-deploy/contracts/PlaceRent.sol
+++ rent-a-place/truffle-deploy/contracts/PlaceRent.sol
@@ -3,11 +3,12 @@ pragma solidity ^0.4.23;
 contract PlaceRent {
 
     struct Rent {
+        bool isValid;
         uint init;
         uint end;
     }
 
-    Rent[] public rent;
+    Rent[] public rents;
     mapping (uint => uint) private rentToPlace;
     mapping (uint => uint) private rentPlaceCounter;
     mapping (uint => address) private rentToPerson;
@@ -15,22 +16,30 @@ contract PlaceRent {
     constructor() public {
     }
 
-    function rentPlace(uint8 _placeId, uint _init, uint _end) public {
-        uint rentId = rent.push(Rent(_init, _end));
+    function rentPlace(uint _placeId, uint _init, uint _end) public {
+        uint rentId = rents.push(Rent(true, _init, _end));
         rentToPlace[rentId] = _placeId;
         rentPlaceCounter[_placeId]++;
         rentToPerson[rentId] = msg.sender;
     }
 
-    function getRent(uint8 _rentId) public view returns(uint, uint) {
-        return(rent[_rentId].init, rent[_rentId].end);
+    function getRent(uint _rentId) public view returns(uint, uint) {
+        return(rents[_rentId].init, rents[_rentId].end);
+    }
+
+    function cancelRent(uint _rentId) public {
+        rents[_rentId].isValid = false;
+    }
+
+    function isValidRent(uint _rentId) public view returns(bool) {
+        return(rents[_rentId].isValid);
     }
 
     function getRentByPlace(uint _placeId) external view returns(uint[]) {
         uint[] memory result = new uint[](rentPlaceCounter[_placeId]);
         
         uint counter = 0;
-        for(uint i = 0; i < rent.length; i++) {
+        for(uint i = 0; i < rents.length; i++) {
             if(rentToPlace[i] == _placeId) {
                 result[counter] = i;
                 counter ++;
